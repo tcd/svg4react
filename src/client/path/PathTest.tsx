8fd9472aeabce9f07b3303710029afdf69582e78
Clay Dunston <claydunston@gmail.com>
2022-09-18 15:04:03 -0500

    Example #2

--- src/client/path/PathTest.tsx
+++ src/client/path/PathTest.tsx
@@ -1,11 +1,17 @@
-import { Example1 } from "./examples"
+import {
+    Example1,
+    Example2,
+} from "./examples"
 
 export const PathTest = (_props: unknown): JSX.Element => {
     return (
-        <div className="container-fluid">
+        <div className="container">
             <div className="row">
                 <div className="col">
                     <Example1 />
+                </div>
+                <div className="col">
+                    <Example2 />
                 </div>
             </div>
         </div>
--- src/client/path/examples/Example2.tsx
+++ src/client/path/examples/Example2.tsx
@@ -2,7 +2,8 @@ import { Path, PathProps } from "@src/lib/components"
 
 const pathProps: PathProps = {
     id: "test-number-1",
-    fill: "red",
+    fill: "none",
+    stroke: "red",
     commands: [
         { cmd: "M", args: [[              10,30]] },
         { cmd: "A", args: [[20,20, 0,0,1, 50,30]] },
@@ -14,12 +15,13 @@ const pathProps: PathProps = {
 }
 
 /**
- * @see https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#example
+ * @see https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#using_d_as_a_css_property
  */
-export const Example1 = (_props: unknown): JSX.Element => {
+export const Example2 = (_props: unknown): JSX.Element => {
     return (
         <svg
             // style={{ width: "100px", height: "100px" }}
+            id="svg_css_ex1"
             width="100px"
             height="100px"
             viewBox="0 0 100 100"
