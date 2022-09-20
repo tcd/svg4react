4c2a7cc5c1c3860ce3ce93fe9c9eb90a513e36eb
Clay Dunston <claydunston@gmail.com>
2022-09-20 09:22:58 -0500

    Fixing up cleaning script

--- etc/scripts/clean.mjs
+++ etc/scripts/clean.mjs
@@ -1,29 +1,39 @@
 /* eslint-disable no-console */
 
 // import { join } from "path"
-// import { readFileSync } from "fs"
+// import { rmSync } from "fs"
 import rimraf from "rimraf"
-import globby from "globby"
+import { globby } from "globby"
 
-
-const main = async () => {
-    globby(["./build/**/*", "!.keep"])
+const main = () => {
+    const targetPaths = [
+        "./build/tsc/**",
+        "./build/api-extractor/*",
+        "!.keep",
+    ]
+    globby(targetPaths)
         .then((paths) => {
             paths.map((item) => {
                 rimraf.sync(item)
+                // rmSync(item, { recursive: true, force: true })
             })
+        })
+        .catch((error) => {
+            console.error(error)
         })
 }
 
-(async () => {
-    main()
-        .then((_res) => {
-            // @ts-ignore:next-line
-            // if (res) { console.log(res) }
-            process.exit(0)
-        })
-        .catch((error) => {
-            if (error) { console.error(error) }
-            process.exit(1)
-        })
-})()
+main()
+
+// (async () => {
+//     main()
+//         .then((_res) => {
+//             // @ts-ignore:next-line
+//             // if (res) { console.log(res) }
+//             process.exit(0)
+//         })
+//         .catch((error) => {
+//             if (error) { console.error(error) }
+//             process.exit(1)
+//         })
+// })()
