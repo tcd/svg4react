1f5541c084638cc3fc55b5e8776e400439b67b3a
Clay Dunston <claydunston@gmail.com>
2022-09-21 18:02:43 -0500

    Hell yeah

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-export * from "./lib"
+export * from "./utils"
+
+export { default as Animate } from "./Animate"
+export * from "./Animate"
+
+export { default as AnimateMotion } from "./AnimateMotion"
+export * from "./AnimateMotion"
+
+export { default as AnimateTransform } from "./AnimateTransform"
+export * from "./AnimateTransform"
+
+export { default as Circle } from "./Circle"
+export * from "./Circle"
+
+export { default as Defs } from "./Defs"
+export * from "./Defs"
+
+export { default as G } from "./G"
+export * from "./G"
+
+export { default as LinearGradient } from "./LinearGradient"
+export * from "./LinearGradient"
+
+export { default as Path } from "./Path"
+export * from "./Path"
+
+export { default as Rect } from "./Rect"
+export * from "./Rect"
+
+export { default as Stop } from "./Stop"
+export * from "./Stop"
+
+export { default as Svg } from "./Svg"
+export * from "./Svg"
