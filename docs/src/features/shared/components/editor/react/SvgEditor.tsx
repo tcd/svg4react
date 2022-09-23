3ae2c903343c19ea3bc56db653f96c0873b0a0d4
Clay Dunston <claydunston@gmail.com>
2022-09-23 17:39:41 -0500

    [docs] [wip] monaco intellisense

--- docs/src/features/shared/components/editor/react/SvgEditor.tsx
+++ docs/src/features/shared/components/editor/react/SvgEditor.tsx
@@ -1,6 +1,8 @@
-import { forwardRef, useEffect, useRef, useState } from "react"
+import noop from "lodash/noop"
+import React, { forwardRef, useCallback, useEffect, useRef, useState } from "react"
 import { isBlank } from "@mlxb/coolkit"
 import MonacoEditor from "react-monaco-editor"
+
 
 // import monaco from "monaco-editor"
 import { parse } from "@babel/parser"
@@ -13,86 +15,50 @@ import { monacoApi, MonacoApi, DarkPlusMonacoTheme } from ".."
 
 export interface SvgEditorProps {
     code: string
+    dev?: boolean
+    onChange?: (value: string, event: monacoApi.editor.IModelContentChangedEvent) => void
 }
 
-export const SvgEditor = forwardRef(function SvgEditor(props: SvgEditorProps, ref) {
+export const SvgEditor = (props: SvgEditorProps) => {
 
     const editorRef = useRef<MonacoEditor>()
 
-    // const [reactDefinitionFile, setReactDefinitionFile] = useState<string>()
-    // const [fetchError, setFetchError] = useState<string>()
+    let {
+        code,
+        dev = false,
+        onChange = noop,
+    } = props
 
-    // const shouldFetch = isBlank(reactDefinitionFile) && isBlank(fetchError)
+    const handleWillMount = (monaco: MonacoApi): void => {
+        monaco.editor.defineTheme("dark-plus", DarkPlusMonacoTheme)
+    }
 
-    // useEffect(() => {
-    //     if (!shouldFetch) {
-    //         return
+    const handleDidMount = (editor: monacoApi.editor.IStandaloneCodeEditor, monaco: MonacoApi) => {
+        _handleDidMount(editor, monaco)
+        if (dev) {
+            editorRef?.current?.editor?.trigger("handleClick", "editor.action.inspectTokens", {})
+        }
+    }
+
+    const handleChange = (value: string, event: monacoApi.editor.IModelContentChangedEvent) => {
+        onChange(value, event)
+    }
+
+    //     const handleClick = (): void => {
+    //
+    //         const editor = editorRef?.current?.editor
+    //         if (!!!editor) {
+    //             return null
+    //         }
+    //
+    //         editor.trigger("handleClick", "editor.action.inspectTokens", {})
+    //         // editor.focus()
+    //
+    //         // const actions = editor.getSupportedActions().map((a) => a.id)
+    //         // const tokenActions = actions.filter(x => x.includes("developer"));
+    //         // console.log(actions)
+    //         // console.log(tokenActions);
     //     }
-    //     fetch("https://cdn.jsdelivr.net/npm/@types/react@16.9.41/index.d.ts", {
-    //         credentials: "include",
-    //         method: "get",
-    //     })
-    //         .then((body) => {
-    //             return body.text()
-    //         })
-    //         .then((text) => {
-    //             setReactDefinitionFile(text)
-    //             console.log("definition file fetched")
-    //         })
-    //         .catch((e) => {
-    //             console.error(e)
-    //             setFetchError(e)
-    //         })
-    // }, [shouldFetch])
-
-    const editorWillMount = (monaco: MonacoApi): void => {
-        monaco.editor.defineTheme("dark-plus", DarkPlusMonacoTheme)
-    }
-
-    const editorDidMount = (editor: monacoApi.editor.IStandaloneCodeEditor, monaco: MonacoApi) => {
-        // const highlighter = new MonacoJSXHighlighter(monaco, parse, traverse, editor)
-        // highlighter.highlightOnDidChangeModelContent(100)
-        // highlighter.addJSXCommentCommand()
-        monaco.editor.defineTheme("dark-plus", DarkPlusMonacoTheme)
-        monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-            target: monaco.languages.typescript.ScriptTarget.Latest,
-            allowNonTsExtensions: true,
-            moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
-            module: monaco.languages.typescript.ModuleKind.CommonJS,
-            noEmit: true,
-            esModuleInterop: true,
-            jsx: monaco.languages.typescript.JsxEmit.React,
-            reactNamespace: "React",
-            allowJs: true,
-            typeRoots: ["node_modules/@types"],
-        })
-        monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
-            noSemanticValidation: false,
-            noSyntaxValidation: false,
-        })
-        monaco.languages.typescript.typescriptDefaults.addExtraLib(
-            reactDefinitionFile,
-            "file:///node_modules/@react/types/index.d.ts",
-        )
-    }
-
-    const onChange = (value: string, event: monacoApi.editor.IModelContentChangedEvent) => {
-
-    }
-
-    const handleClick = (): void => {
-        // console.log("clicked");
-        // const model = this.monacoRef.editor.getModel();
-        // const value = model.getValue();
-        // alert(value);
-        // this.monacoRef.editor.executeCommand("Developer: Inspect Tokens", "Developer: Inspect Tokens")
-        editorRef.current.editor.trigger("handleClick", "editor.action.inspectTokens", {})
-        // const actions = this.editor.getSupportedActions().map((a) => a.id);
-        // console.log(actions);
-        // const tokenActions = actions.filter(x => x.includes("developer"));
-        // console.log(tokenActions);
-        editorRef.current.editor.focus()
-    }
 
     const options: monacoApi.editor.IStandaloneEditorConstructionOptions = {
         // language: "jsx",
@@ -101,20 +67,60 @@ export const SvgEditor = forwardRef(function SvgEditor(props: SvgEditorProps, re
 
     return (
         <>
-            <button className="btn btn-primary mb-5" onClick={handleClick}>Inspect</button>
+            {/* <button className="btn btn-primary mb-5" onClick={handleClick}>Inspect</button> */}
             <MonacoEditor
                 ref={(node) => { editorRef.current = node }}
                 width="800"
                 height="600"
-                language="typescript"
+                // language="typescript"
                 theme="dark-plus"
-                // theme=""
-                value={props.code}
+                value={code}
                 options={options}
-                onChange={onChange}
-                editorDidMount={editorDidMount}
-                editorWillMount={editorWillMount}
+                onChange={handleChange}
+                editorDidMount={handleDidMount}
+                editorWillMount={handleWillMount}
             />
         </>
     )
-})
+}
+
+// =============================================================================
+// Helpers
+// =============================================================================
+
+const _handleDidMount = (editor: monacoApi.editor.IStandaloneCodeEditor, monaco: MonacoApi) => {
+
+    // -------------------------------------------------------------------------
+    // https://github.com/luminaxster/syntax-highlighter
+    // -------------------------------------------------------------------------
+
+    // const highlighter = new MonacoJSXHighlighter(monaco, parse, traverse, editor)
+    // highlighter.highlightOnDidChangeModelContent(100)
+    // highlighter.addJSXCommentCommand()
+
+    // -------------------------------------------------------------------------
+    // https://github.com/microsoft/monaco-editor/issues/264#issuecomment-654578687
+    // -------------------------------------------------------------------------
+
+    monaco.editor.defineTheme("dark-plus", DarkPlusMonacoTheme)
+    monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+        target: monaco.languages.typescript.ScriptTarget.Latest,
+        allowNonTsExtensions: true,
+        moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+        module: monaco.languages.typescript.ModuleKind.CommonJS,
+        noEmit: true,
+        esModuleInterop: true,
+        jsx: monaco.languages.typescript.JsxEmit.React,
+        reactNamespace: "React",
+        allowJs: true,
+        typeRoots: ["node_modules/@types"],
+    })
+    monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+        noSemanticValidation: false,
+        noSyntaxValidation: false,
+    })
+    monaco.languages.typescript.typescriptDefaults.addExtraLib(
+        reactDefinitionFile,
+        "file:///node_modules/@react/types/index.d.ts",
+    )
+}
