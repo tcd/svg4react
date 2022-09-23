703e426d6c7a4f6227ec6c9c9fa47612e60c11ae
Clay Dunston <claydunston@gmail.com>
2022-09-22 19:42:16 -0500

    [docs] [WIP] build script for prod

---
+++ docs/webpack.prod.js
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require("path")
+// const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
+
+const PROJECT_ROOT = "."
+
+/**
+ * See [Webpack Configuration docs](https://webpack.js.org/configuration/) for more information.
+ *
+ * @type {import("webpack").Configuration}
+ */
+const webpackConfig = {
+    mode: "production",
+    devtool: false,
+    /** @type {import("webpack").Configuration} */
+    entry: path.resolve(PROJECT_ROOT, "src", "index.tsx"),
+    output: {
+        path: path.resolve(PROJECT_ROOT, "dist"),
+        publicPath: "/",
+        filename: "bundle.js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(PROJECT_ROOT, "src", "index.html"),
+            hash: true,
+            inject: true,
+        }),
+    ],
+    resolve: {
+        extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+        modules: [__dirname, "src", "node_modules"],
+        plugins: [
+            new TsconfigPathsPlugin.TsconfigPathsPlugin(),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                enforce: "pre",
+                test: /\.tsx?$/,
+                use: "source-map-loader",
+            },
+        ],
+    },
+}
+
+module.exports = webpackConfig
