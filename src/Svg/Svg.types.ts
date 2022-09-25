f6d07acde4935b42c0713e5f241c69b34b4b8c1c
Clay Dunston <claydunston@gmail.com>
2022-09-25 16:18:21 -0500

    [lib] working on a viewbox shorthand.

--- src/Svg/Svg.types.ts
+++ src/Svg/Svg.types.ts
@@ -1,5 +1,15 @@
 import { SVGProps } from "react"
 
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/viewBox
+ */
+export type ViewBox = [
+    minX: number,
+    minY: number,
+    width: number,
+    height: number,
+]
+
 export interface SvgProps extends SVGProps<SVGSVGElement> {
     /**
      * Assigned to both `width` and `height`
---
+++ src/utils/types/IViewBox.ts
@@ -0,0 +1,9 @@
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/viewBox
+ */
+export interface IViewBox {
+    minX: number
+    minY: number
+    width: number
+    height: number
+}
