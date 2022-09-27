1d3b564533b67803f4c5727b382d424effa3b056
Clay Dunston <claydunston@gmail.com>
2022-09-27 17:40:51 -0500

    [docs] build for gh-pages/vercel

--- docs/config/webpack.config.prod.js
+++ docs/config/webpack.config.prod.js
@@ -2,7 +2,7 @@
 
 const path = require("path")
 const webpack = require("webpack")
-const CopyPlugin = require("copy-webpack-plugin")
+// const CopyPlugin = require("copy-webpack-plugin")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer")
 
