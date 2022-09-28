e8767aa7f906dfe7ee9622c78616200fbb9a5275
Clay Dunston <claydunston@gmail.com>
2022-09-28 17:59:51 -0500

    [docs] update markdown styles

--- docs/src/features/core/components/pages/ChangelogPage.tsx
+++ docs/src/features/core/components/pages/ChangelogPage.tsx
@@ -1,19 +1,22 @@
-import ReactMarkdown from "react-markdown"
+import { useSelector } from "react-redux"
 import { Box, Paper } from "@mui/material"
+import ReactMarkdown from "react-markdown"
 
 import { DocsSx } from "@app/theme"
+import { Selectors } from "@app/state"
 import { Page } from "@app/features/shared"
 import changelogMarkdown from "../../../../../../CHANGELOG.md"
 
 export const ChangelogPage = (_props: unknown): JSX.Element => {
+
+    const darkModeEnabled = useSelector(Selectors.Core.darkModeEnabled)
+
+    const markdownClassName = darkModeEnabled ? "markdown-body-dark" : "markdown-body-light"
+
     return (
         <Page title="Changelog">
-            <Paper
-                id="changelog"
-                elevation={3}
-                sx={DocsSx.Changelog.root}
-            >
-                <Box className="markdown-body">
+            <Paper elevation={3} sx={DocsSx.Changelog.root}>
+                <Box className={markdownClassName} >
                     <ReactMarkdown>
                         {changelogMarkdown}
                     </ReactMarkdown>
--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client"
 import { App } from "./app"
 
 import "./styles/main.scss"
-import "./styles/markdown/github-markdown-light.css"
 
 const container = document.getElementById("root")
 const root = createRoot(container)
