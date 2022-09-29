457c32a89c82d6041869adf7985f7c2ca018f8a1
Clay Dunston <claydunston@gmail.com>
2022-09-29 17:03:55 -0500

    [lib] Update `SvgProps`

--- src/Svg/Svg.tsx
+++ src/Svg/Svg.tsx
@@ -2,17 +2,13 @@ import { forwardRef } from "react"
 import type { SvgProps } from "./Svg.types"
 
 const defaultProps: Partial<SvgProps> = {
+    // @ts-ignore: next-line
     xmlnsXlink: "http://www.w3.org/1999/xlink",
+    // @ts-ignore: next-line
     xmlns: "http://www.w3.org/2000/svg",
-    version: "1.1",
-    // width: "100px",
-    // height: "100px",
-    viewBox: "0 0 100 100",
 }
 
 /**
- * @public
- *
  * A wrapper for the [`<svg>`](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/svg) element.
  *
  * The `<svg>` element is a container that defines a new coordinate system and [viewport](https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/viewBox).
@@ -22,22 +18,27 @@ const defaultProps: Partial<SvgProps> = {
 const Svg = forwardRef<SVGSVGElement, SvgProps>(
     function Svg(props, ref) {
 
-        props = {
+        const {
+            size = null,
+            vb = [],
+            ...otherProps
+        } = {
             ...defaultProps,
             ...props,
         }
 
-        const {
-            size = null,
-            ...otherProps
-        } = props
+        if (!otherProps.viewBox) {
+            if (vb.length === 1) { otherProps.viewBox = `0 0 ${vb[0]} ${vb[0]}` }
+            if (vb.length === 2) { otherProps.viewBox = `0 0 ${vb[0]} ${vb[1]}` }
+        }
 
         if (size) {
-            otherProps.width = size
-            otherProps.height = size
+            otherProps.width  = otherProps?.width ?? size
+            otherProps.height = otherProps.height ?? size
         }
 
         return (
+            // @ts-ignore: next-line
             <svg
                 ref={ref}
                 {...otherProps}
--- src/Svg/Svg.types.ts
+++ src/Svg/Svg.types.ts
@@ -1,18 +1,70 @@
 import { SVGProps } from "react"
+import type { SvgPresentationAttributes } from "../utils"
 
-/**
- * @see https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/viewBox
- */
-export type ViewBox = [
-    minX: number,
-    minY: number,
-    width: number,
-    height: number,
-]
-
-export interface SvgProps extends SVGProps<SVGSVGElement> {
+type SvgAttributes = {
     /**
-     * Assigned to both `width` and `height`
+     * Shorthand for the setting the last two values of `viewBox`
+     *
+     * If only one value is passed, it will be assigned to both `width` and `height`
+     *
+     * `minX` and `minY` are set to 0, resulting in `"0 0 width height"`.
+     *
+     * Will **not** override `viewBox` if both are passed.
+     */
+    vb?: [width: number, height?: number]
+    /**
+     * Shorthand for setting both `width` and `height`.
      */
     size?: string
-}
+    /**
+     * The SVG viewport coordinates for the current SVG fragment.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/viewBox
+     */
+    viewBox?: string
+    /**
+     * How the `svg` fragment must be deformed if it is displayed with a different aspect ratio.
+     *
+     * @default "xMidYMid meet"
+     */
+    preserveAspectRatio?: "none" | `${"xMidYMin"|"xMinYMin"|"xMaxYMin"|"xMinYMid"|"xMidYMid"|"xMaxYMid"|"xMinYMax"|"xMidYMax"|"xMaxYMax"} ${"meet"|"slice"}`
+} & Pick<SvgPresentationAttributes,
+    | "clipPath"
+    | "clipRule"
+    | "color"
+    | "colorInterpolation"
+    | "cursor"
+    | "display"
+    | "fill"
+    | "fillOpacity"
+    | "fillRule"
+    | "filter"
+    | "mask"
+    | "opacity"
+    | "pointerEvents"
+    | "shapeRendering"
+    | "stroke"
+    | "strokeDasharray"
+    | "strokeDashoffset"
+    | "strokeLinecap"
+    | "strokeLinejoin"
+    | "strokeMiterlimit"
+    | "strokeOpacity"
+    | "strokeWidth"
+    | "transform"
+    | "vectorEffect"
+    | "visibility"
+>
+
+type Deprecated =
+    | "baseProfile"
+    | "contentScriptType"
+    | "contentStyleType"
+    | "version"
+
+/**
+ * @see [MDN - `<svg>`](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/svg)
+ */
+export type SvgProps =
+    Omit<SVGProps<SVGSVGElement>, keyof SvgAttributes | Deprecated | "xmlns" | "xmlnsXlink">
+    & SvgAttributes
