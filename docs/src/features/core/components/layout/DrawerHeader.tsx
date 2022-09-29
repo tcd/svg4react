ca237d2a0d94e6cdf71a95a125e38a9e36eaf0a6
Clay Dunston <claydunston@gmail.com>
2022-09-29 18:13:48 -0500

    [docs] update drawer styles

--- docs/src/features/core/components/layout/DrawerHeader.tsx
+++ docs/src/features/core/components/layout/DrawerHeader.tsx
@@ -1,17 +1,25 @@
-import { Box } from "@mui/material"
+import { Box, ButtonBase } from "@mui/material"
+import { useNavigate } from "react-router-dom"
 
 import { DocsSx } from "@app/theme"
 import { Logo } from "@app/features/shared"
 
 export const DrawerHeader = (_props: unknown): JSX.Element => {
+
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        navigate("/")
+    }
+
     return (
-        <Box sx={DocsSx.Layout.drawerHeader}>
+        <ButtonBase sx={DocsSx.Layout.drawerHeader} onClick={handleClick}>
             <Box sx={DocsSx.Layout.drawerLogo}>
                 <Logo />
             </Box>
             {/* <Typography>
                 svg4react
             </Typography> */}
-        </Box>
+        </ButtonBase>
     )
 }
