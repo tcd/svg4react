00ec2121001fa5271169f59ddc9de53070e58a60
Clay Dunston <claydunston@gmail.com>
2022-10-03 09:31:42 -0500

    [docs] Update pages for `Rect` & `LinearGradient`

--- packages/docs/src/features/demos/linear-gradient/components/LinearGradientPage.tsx
+++ packages/docs/src/features/demos/linear-gradient/components/LinearGradientPage.tsx
@@ -1,11 +1,15 @@
-import { Page } from "@app/features/shared"
+import { Stack } from "@mui/material"
 
+import { Page, ComponentOverview } from "@app/features/shared"
 import { LinearGradientDemo } from "./LinearGradientDemo"
 
 export const LinearGradientPage = (_props: unknown): JSX.Element => {
     return (
         <Page title="<LinearGradient>">
-            <LinearGradientDemo />
+            <Stack spacing={4}>
+                <ComponentOverview componentName="LinearGradient" />
+                <LinearGradientDemo />
+            </Stack>
         </Page>
     )
 }
--- packages/docs/src/features/demos/rect/components/RectPage.tsx
+++ packages/docs/src/features/demos/rect/components/RectPage.tsx
@@ -1,11 +1,15 @@
-import { Page } from "@app/features/shared"
+import { Stack } from "@mui/material"
 
+import { Page, ComponentOverview } from "@app/features/shared"
 import { RectDemo } from "./RectDemo"
 
 export const RectPage = (_props: unknown): JSX.Element => {
     return (
         <Page title="Rect">
-            <RectDemo />
+            <Stack spacing={4}>
+                <ComponentOverview componentName="Rect" />
+                <RectDemo />
+            </Stack>
         </Page>
     )
 }
