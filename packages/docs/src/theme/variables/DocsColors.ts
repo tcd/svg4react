5d81c135a2dbaede523bf52fc95d63dafb43fdc1
Clay Dunston <claydunston@gmail.com>
2022-10-04 14:06:45 -0500

    [docs] research new logo color

--- packages/docs/src/theme/variables/DocsColors.ts
+++ packages/docs/src/theme/variables/DocsColors.ts
@@ -1,5 +1,37 @@
+/**
+ * ## See
+ *
+ * - [PineTools - Blend Colors](https://pinetools.com/blend-colors)
+ */
 export const DocsColors = <const>{
     svgOrange: "#ffb13b",
     reactBlue: "#61dbfb",
     materialBlue: "#1976d2",
+    puke: "#b0b043",
+}
+
+// =============================================================================
+// Scratch
+// =============================================================================
+
+const scratchColors: Record<string, string[]> = {
+    green: [
+        "#b0c69b", // #ffb13b (50%) + #61dbfb (50%)
+        "#a8c8a4", // #ffb13b (55%) + #61dbfb (45%)
+        "#91e27f", // https://colorkit.co/color-palette-generator/ffb13b-91e27f-61dbfb/
+        "#94efc0", // https://colorkit.co/color-palette-generator/ffb13b-94efc0-61dbfb/
+        "#64C2A9", // http://colormind.io/
+        "#B0B043", // http://colormind.io/
+        "#4CB885", // http://colormind.io/
+    ],
+    red: [
+        "#910900", // https://colorkit.co/color-palette-generator/ffb13b-910900-61dbfb/
+    ],
+    blue: [
+        "#61dbfb", // react
+        "#1976d2", // Material UI default
+    ],
+    orange: [
+        "#ffb13b", // SVG Logo
+    ],
 }
