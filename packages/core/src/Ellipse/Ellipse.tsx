7e6a8c866465486399b965b264bead7f22f00e3c
Clay Dunston <claydunston@gmail.com>
2022-10-04 12:38:27 -0500

    [core] Expand in doc comment for `Ellipse` component

--- packages/core/src/Ellipse/Ellipse.tsx
+++ packages/core/src/Ellipse/Ellipse.tsx
@@ -5,6 +5,8 @@ import type { EllipseProps } from "./Ellipse.types"
  * A wrapper for the [`<ellipse>`](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/ellipse) element.
  *
  * The `<ellipse>` element is an SVG basic shape, used to create ellipses based on a center coordinate, and both their x and y radius.
+ *
+ * **Note**: Ellipses are unable to specify the exact orientation of the ellipse (if, for example, you wanted to draw an ellipse tilted at a 45 degree angle), but they can be rotated by using the `transform` attribute.
  */
 const Ellipse = forwardRef<SVGEllipseElement, EllipseProps>(function Ellipse(props: EllipseProps, ref) {
 
