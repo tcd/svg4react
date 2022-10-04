a4e7c59d0e14b9499498596b51286af3c74c4c43
Clay Dunston <claydunston@gmail.com>
2022-10-04 13:02:10 -0500

    [docs] update `PrismLanguageProps`

--- packages/docs/src/util/prism-language-props.ts
+++ packages/docs/src/util/prism-language-props.ts
@@ -1,10 +1,32 @@
 import type { Grammar } from "prismjs"
 
+// /**
+//  * @see [Extending Prism - Language definitions](https://prismjs.com/extending.html)
+//  */
+// export const PrismLanguageProps: Grammar = {
+//     operator: /\||\./,
+//     boolean: /\b(?:false|true)\b/,
+//     keyword: /\$\{|\}/,
+//     string: /("[^"]+")|(`)/,
+//     property: {
+//         pattern: /[a-zA-Z_]([a-zA-Z_0-9]+)?\??:/,
+//         inside: {
+//             type: /:|\?/,
+//         },
+//     },
+//     type: {
+//         pattern: /(\b(string|number)\b)/,
+//         inside: {
+//             operator: /\./,
+//         },
+//     },
+// }
+
 /**
  * @see [Extending Prism - Language definitions](https://prismjs.com/extending.html)
  */
 export const PrismLanguageProps: Grammar = {
-    operator: /\|/,
+    operator: /\||\./,
     boolean: /\b(?:false|true)\b/,
     keyword: /\$\{|\}/,
     string: /("[^"]+")|(`)/,
@@ -14,5 +36,12 @@ export const PrismLanguageProps: Grammar = {
             type: /:|\?/,
         },
     },
-    type: /\b(string|number)\b/,
+    type: {
+        // pattern: /(\b(string|number)\b)|(\.[a-zA-Z_]+)|([a-zA-Z_]+\.)/,
+        pattern: /(\b(string|number)\b)|((?<=\.)[a-zA-Z_]+)|([a-zA-Z_]+(?=\.))/,
+        greedy: true,
+        inside: {
+            operator: /\./,
+        },
+    },
 }
