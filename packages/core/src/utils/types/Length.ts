97665b181f8753137742f0c702033f81f3087d89
Clay Dunston <claydunston@gmail.com>
2022-10-05 18:27:13 -0500

    [core] [FMT]

--- packages/core/src/utils/types/Length.ts
+++ packages/core/src/utils/types/Length.ts
@@ -20,7 +20,7 @@ export type Unit =
 /**
  * @public
  *
- * A `length` is a distance measurement, given as a number along with a unit. Lengths are specified in one of two ways.
+ * A `length` is a distance measurement, given as a number along with a unit.
  *
  * @see [MDN Web Docs - `length`](https://developer.mozilla.org/en-US/docs/Web/SVG/Content_type#length)
  * @see [MDN Web Docs - `SVGLength`](https://developer.mozilla.org/en-US/docs/Web/API/SVGLength)
