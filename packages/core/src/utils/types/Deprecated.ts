3f56dfdb14e12f8adf9fe2e44d09b0f8865c6bde
Clay Dunston <claydunston@gmail.com>
2022-10-07 11:05:32 -0500

    [core] various WIP type definitions

---
+++ packages/core/src/utils/types/Deprecated.ts
@@ -0,0 +1,54 @@
+export namespace Deprecated {
+    export type Components = {
+        "tref": never
+        "altGlyph": never
+        "altGlyphDef": never
+        "altGlyphItem": never
+        "animateColor": never
+        "cursor": never
+        "font": never
+        "font-face": never
+        "font-face-format": never
+        "font-face-name": never
+        "font-face-src": never
+        "font-face-uri": never
+        "glyph": never
+        "glyphRef": never
+        "hkern": never
+        "missing-glyph": never
+        "vkern": never
+    }
+    export type Attributes = {
+        /**
+         * used by:
+         *
+         * - `animate`
+         * - `animateColor`
+         * - `animateTransform`
+         * - `set`
+         *
+         * @see https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/attributeType
+         * @see https://docs.w3cub.com/svg/attribute/color-rendering
+         */
+        attributeType: never
+        /**
+         * @see https://docs.w3cub.com/svg/attribute/color-rendering
+         */
+        colorRendering: never
+        /**
+         * used by:
+         *
+         * - `filter`
+         *
+         * @see https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/filterRes
+         */
+        filterRes: never
+        xmlSpace: never
+        xlinkArcrole: never
+        xlinkShow: never
+        xlinkTitle: never
+        xlinkType: never
+        xmlBase: never
+        xmlLang: never
+    }
+}
