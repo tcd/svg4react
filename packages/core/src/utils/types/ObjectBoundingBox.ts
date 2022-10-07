0af571725c2746fdf731ebc22f544f7ad44471d2
Clay Dunston <claydunston@gmail.com>
2022-10-07 10:01:40 -0500

    [core] rename (unused) `IViewBox` to `ObjectBoundingBox`

--- packages/core/src/utils/types/ObjectBoundingBox.ts
+++ packages/core/src/utils/types/ObjectBoundingBox.ts
@@ -1,7 +1,8 @@
 /**
  * @see [MDN - `viewBox`](https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/viewBox)
+ * @see [MDN - `filterUnits`](https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/filterUnits)
  */
-export interface IViewBox {
+export interface ObjectBoundingBox {
     minX: number
     minY: number
     width: number
