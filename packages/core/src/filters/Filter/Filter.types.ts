9533f13253171dbf409d5d50b81d8e5b3fdd50be
Clay Dunston <claydunston@gmail.com>
2022-10-07 10:00:58 -0500

    [core] little bit of work on filter

--- packages/core/src/filters/Filter/Filter.types.ts
+++ packages/core/src/filters/Filter/Filter.types.ts
@@ -2,7 +2,18 @@ import type { SVGProps } from "react"
 import type { CommonSvgPresentationAttributes } from "../../utils"
 
 export type _FilterAttributes = {
-
+    /**
+     * Defines the coordinate system for the attributes `x`, `y`, `width`, and `height`.
+     *
+     * @default "objectBoundingBox"
+     */
+    filterUnits?: "userSpaceOnUse" | "objectBoundingBox"
+    /**
+     * Specifies the coordinate system for the various length values within the filter primitives and for the attributes that define the filter primitive subregion.
+     *
+     * @default "userSpaceOnUse"
+     */
+    primitiveUnits?: "userSpaceOnUse" | "objectBoundingBox"
 }
 
 export type FilterAttributes = _FilterAttributes & CommonSvgPresentationAttributes
