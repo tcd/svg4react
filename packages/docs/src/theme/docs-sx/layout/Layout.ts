f6551cd496e1fe184bfc324f58948b88c38fd109
Clay Dunston <claydunston@gmail.com>
2022-10-09 18:03:01 -0500

    [docs] fix width of main layout section on mobile

--- packages/docs/src/theme/docs-sx/layout/Layout.ts
+++ packages/docs/src/theme/docs-sx/layout/Layout.ts
@@ -1,12 +1,26 @@
-import type { ISxProps as SxProps } from "@app/theme/helpers"
+import type {
+    ISxProps as SxProps,
+    ITheme as Theme,
+} from "@app/theme/helpers"
+import { ThemeVars } from "@app/theme/variables"
 
 const root: SxProps = {
     display: "flex",
 }
 
-const main: SxProps = {
-    flexGrow: 1,
-    p: 3,
+const main: SxProps = (theme: Theme) => {
+    const padding = theme.spacing(3)
+    // console.log(theme.spacing(3))
+    return {
+        flexGrow: 1,
+        p: 3,
+        [theme.breakpoints.down("laptop")]: {
+            width: `calc(100vw - ${padding} - ${padding})`,
+        },
+        [theme.breakpoints.up("laptop")]: {
+            width: `calc(100vw - ${ThemeVars.drawerWidth} - ${padding} - ${padding})`,
+        },
+    }
 }
 
 export const Layout = {
