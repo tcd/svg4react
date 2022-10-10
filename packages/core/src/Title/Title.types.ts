9fe691350b54b7573afbfb79698feb0d2db0cc25
Clay Dunston <claydunston@gmail.com>
2022-10-10 17:19:34 -0500

    [core] Add `Title` component (or the skeleton of it)

---
+++ packages/core/src/Title/Title.types.ts
@@ -0,0 +1,6 @@
+import type { ISVGProps } from "../utils"
+
+/**
+ * @see [MDN Web Docs - `<title>`](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/title)
+ */
+export type TitleProps = ISVGProps<SVGTitleElement>
