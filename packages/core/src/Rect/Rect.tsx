412fdd3727e7c741963ae60b208da5010b5a7fee
Clay Dunston <claydunston@gmail.com>
2022-10-10 17:50:22 -0500

    [core] [FMT]

--- packages/core/src/Rect/Rect.tsx
+++ packages/core/src/Rect/Rect.tsx
@@ -4,8 +4,7 @@ import processProps from "./process-rect-props"
 /**
  * A wrapper for the [`<rect>`](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/rect) element.
  *
- * The `<rect>` element is a [basic SVG shape](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes) that draws rectangles,
- * defined by their position, width, and height.
+ * The `<rect>` element is a [basic SVG shape](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes) that draws rectangles, defined by their position, width, and height.
  */
 const Rect = (props: RectProps): JSX.Element => {
 
