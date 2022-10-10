efcdf308c104b3e59eae5a6f40c6c71733f867ad
Clay Dunston <claydunston@gmail.com>
2022-10-10 18:40:19 -0500

    [docs] BURGER

--- packages/docs/src/features/test/components/burger/v4/V4.tsx
+++ packages/docs/src/features/test/components/burger/v4/V4.tsx
@@ -13,21 +13,23 @@ export const V4 = (_props: unknown): JSX.Element => {
     const sx = checked ? SX.checked : SX.default
 
     return (
-        <Box className="burger-wrapper" sx={sx} onClick={onClick}>
-            <Svg id="icon" viewBox="0 16 32 32" size="100%">
-                <Defs>
-                    <filter id="gooey">
-                        <feGaussianBlur {...gooey.gaussianBlur} />
-                        <feColorMatrix  {...gooey.colorMatrix} />
-                        <feBlend        {...gooey.blend} />
-                    </filter>
-                </Defs>
-                <G className="icon">
-                    <Path className="top"    d={paths.top} />
-                    <Path className="middle" d={paths.middle} />
-                    <Path className="bottom" d={paths.bottom} />
-                </G>
-            </Svg>
+        <Box sx={{ width: "200px", height: "200px" }}>
+            <Box className="burger-wrapper" sx={sx} onClick={onClick}>
+                <Svg id="icon" viewBox="0 16 32 32" size="100%">
+                    <Defs>
+                        <filter id="gooey">
+                            <feGaussianBlur {...gooey.gaussianBlur} />
+                            <feColorMatrix  {...gooey.colorMatrix} />
+                            <feBlend        {...gooey.blend} />
+                        </filter>
+                    </Defs>
+                    <G className="icon">
+                        <Path className="top"    d={paths.top} />
+                        <Path className="middle" d={paths.middle} />
+                        <Path className="bottom" d={paths.bottom} />
+                    </G>
+                </Svg>
+            </Box>
         </Box>
     )
 }
