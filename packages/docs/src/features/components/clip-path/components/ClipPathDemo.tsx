3dece558c81e14575d0b539bac0a39b8cb3ded23
Clay Dunston <claydunston@gmail.com>
2022-10-11 16:48:31 -0500

    [docs] update `ClipPath` demo

--- packages/docs/src/features/components/clip-path/components/ClipPathDemo.tsx
+++ packages/docs/src/features/components/clip-path/components/ClipPathDemo.tsx
@@ -1,16 +1,20 @@
-import { Svg, Use, Circle, Path, ClipPath } from "svg4react"
+import { useSelector } from "react-redux"
+import { Svg, ClipPath, Use, Circle, Path, Animate } from "svg4react"
 
+import { Selectors } from "@app/state"
 import { LiveDemo } from "@app/features/live-demo"
 
 export const ClipPathDemo = (_props: unknown): JSX.Element => {
 
-    const scope = { Svg, Use, Circle, Path, ClipPath }
+    const darkModeEnabled = useSelector(Selectors.Core.darkModeEnabled)
+
+    const scope = { Svg, ClipPath, Use, Circle, Path, Animate }
 
     return (
         <LiveDemo
             id="clip-path-demo-1"
             title="Demo"
-            code={code}
+            code={code(darkModeEnabled)}
             scope={scope}
         />
     )
@@ -18,39 +22,39 @@ export const ClipPathDemo = (_props: unknown): JSX.Element => {
 
 // =============================================================================
 
-const code = `
-import { Svg, Use, Circle, Path, ClipPath } from "svg4react"
+const code = (darkMode: boolean) => {
 
-const styles = /*CSS*/\`
-    /* With a touch of CSS for browsers who implemented the r Geometry Property. */
-    @keyframes openYourHeart {
-        from { r: 0;    }
-        to   { r: 60px; }
-    }
+    // const color = darkMode ? "white" : "black"
+    const color = "black"
 
-    #myClip circle {
-        animation: openYourHeart 10s infinite;
-    }
-\`
+    return `
+import { Svg, ClipPath, Use, Circle, Path, Animate } from "svg4react"
 
 render(
-    <>
-        <style>{styles}</style>
-        <Svg size="75%" vb={[100]}>
-            <ClipPath id="myClip">
-                {/* Everything outside the circle will be clipped and therefore invisible. */}
-                <Circle coordinates={[40,35]} r="35" />
-            </ClipPath>
+    <Svg size="80%" vb={[100]}>
+        <ClipPath id="myClip">
+            {/* Everything outside the circle will be clipped and therefore invisible. */}
+            <Circle coordinates={[40,35]}>
+                <Animate
+                    attributeName="r"
+                    from="0px"
+                    to="60px"
+                    dur="10s"
+                    repeatCount="indefinite"
+                />
+            </Circle>
+        </ClipPath>
 
-            {/* The original black heart, for reference */}
-            <Path
-                id="heart"
-                d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z"
-            />
+        {/* The original ${color} heart, for reference */}
+        <Path
+            id="heart"
+            // fill="${color}"
+            d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z"
+        />
 
-            {/* Only the portion of the red heart inside the clip circle is visible. */}
-            <Use clipPath="url(#myClip)" href="#heart" fill="red" />
-        </Svg>
-    </>
+        {/* Only the portion of the red heart inside the clip circle is visible. */}
+        <Use clipPath="url(#myClip)" href="#heart" fill="red" />
+    </Svg>
 )
 `
+}
